47f211e42ddaa03a62d6e45c4452fb2029db475e
Abhijeet Dalal <abhijeetdalal@gmail.com>
2014-02-28 22:52:51 +0530

    Initial draft for iBatis example.

---
+++ iBatisProject/src/java/test/ad/rnd/main/IbatisInsert.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package test.ad.rnd.main;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.sql.SQLException;
+
+import com.ibatis.common.resources.Resources;
+import com.ibatis.sqlmap.client.SqlMapClient;
+import com.ibatis.sqlmap.client.SqlMapClientBuilder;
+
+/**
+ * @author Abhijeet Dalal
+ * 
+ */
+public class IbatisInsert {
+	public static void main(String[] args) throws IOException, SQLException {
+		InputStream inputStream = IbatisInsert.class.getResourceAsStream("resources/SqlMapConfig.xml");
+		
+		Reader rd = new InputStreamReader(inputStream);
+		//Reader rd = Resources.getResourceAsReader("SqlMapConfig.xml");
+		SqlMapClient smc = SqlMapClientBuilder.buildSqlMapClient(rd);
+
+		/* This would insert one record in Employee table. */
+		System.out.println("Going to insert record.....");
+		Employee em = new Employee("Ashita", "Dalal", 5000);
+
+		smc.insert("Employee.insert", em);
+
+		System.out.println("Record Inserted Successfully ");
+
+	}
+}
